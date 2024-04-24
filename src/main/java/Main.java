b4af233de637e67165b7ecb3744466ca3cc05fdc
adyac983 <adchoudh@calpoly.edu>
2024-04-23 21:47:30 -0700

    parsing done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -10,5 +11,15 @@ public class Main {
         for (DataRecord record : dataRecords) {
             System.out.println(record.getHeader() + ") " + record.getCountry() + " : " + record.getNum());
         }
+
+        List<Building> buildings = BuildingParser.parseDataToBuildings(dataRecords);
+
+        // Display the parsed buildings
+        System.out.println("Parsed Buildings:");
+        for (int i = 0; i < buildings.size(); i++) {
+            Building building = buildings.get(i);
+            System.out.println("Building " + (i + 1) + ": Length=" + building.getLength() + ", Breadth=" + building.getBreadth());
+        }
+
     }
 }
