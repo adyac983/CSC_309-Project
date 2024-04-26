c0768e9266a8b7424202f6d46ba112b4bd11ab11
Yud16 <89806105+Yud16@users.noreply.github.com>
2024-04-26 12:21:25 -0700

    code with player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,35 +4,6 @@ import java.util.List;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-
-//public class Main extends JFrame {
-//    public Main() {
-//        //add panels inside here
-//        JPanel mainPanel = new JPanel();
-//        mainPanel.setLayout(null);
-//        GameData.getInstance().setSize(this.getWidth(), this.getHeight());
-//        Building firstBuilding = GameData.getInstance().getCurrBuilding();
-//
-//        BuildingPanel buildingPanel = new BuildingPanel(GameData.getInstance().getBuildings());
-//        Player player = new Player(firstBuilding.getX(),firstBuilding.getY(),10,30);
-//
-//        mainPanel.add(buildingPanel);
-//        mainPanel.add(player);
-//        //GameData.getInstance().addPropertyChangeListener();
-//    }
-//
-//    //right now, code here is mainly for testing that the stuff works
-//    public static void main(String[] args) {
-//        Main main = new Main();
-//        main.setTitle("Building data");
-//        main.setSize(800, 400);
-//        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        main.setVisible(true);
-//
-//    }
-//}
-
-
 public class Main {
     //right now, code here is mainly for testing that the stuff works
     public static void main(String[] args) {
