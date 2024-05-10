5ad76f113562479cfe74606c80b87c3587d29a2c
Safwan Muntasir <smuntasi@calpoly.edu>
2024-05-10 05:36:09 -0700

    crashing of boy

--- src/main/java/BuildingPanel.java
+++ src/main/java/BuildingPanel.java
@@ -1,17 +1,21 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.*;
 
 public class BuildingPanel extends JPanel {
-    private List <Building> buildings;
+    private List<Building> buildings;
     private int maxHeight;
-    private int scrollPaneHeight;
+    private Image backgroundImage;
 
-    public BuildingPanel(List <Building> buildings) {
+    private JComboBox<String> backgroundSelector;
+
+    public BuildingPanel(List<Building> buildings) {
         this.buildings = buildings;
         calculateDimensions();
+        setupBackgroundSelector();
+        loadDefaultBackgroundImage();
     }
 
     void calculateDimensions() {
@@ -23,36 +27,84 @@ public class BuildingPanel extends JPanel {
         }
     }
 
+    void setupBackgroundSelector() {
+        String[] backgroundOptions = {"City", "CO2", "Forest"};
+        backgroundSelector = new JComboBox<>(backgroundOptions);
+        backgroundSelector.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedBackground = (String) backgroundSelector.getSelectedItem();
+                loadBackgroundImage(selectedBackground);
+                repaint();
+            }
+        });
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        backgroundSelector.setPreferredSize(new Dimension(200, backgroundSelector.getPreferredSize().height));
+        add(backgroundSelector); // Add the dropdown menu to the top of the panel
+    }
+
+    void loadDefaultBackgroundImage() {
+        loadBackgroundImage("Background 1"); // Default background
+    }
+
+    void loadBackgroundImage(String selectedBackground) {
+        String bg = selectedBackground.toLowerCase().replace(" ", "_");
+        java.net.URL url = getClass().getResource("/images/" + bg + ".jpg");
+        if (url != null) {
+            ImageIcon bgIcon = new ImageIcon(url);
+            backgroundImage = bgIcon.getImage();
+        } else {
+            // Handle the case when the resource is not found
+            System.out.println("Background image not found: " + bg);
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         GameData.getInstance().recalculate();
         super.paintComponent(g);
-        int x = 20;
-        int x1 = 1;
-        scrollPaneHeight = GameData.getInstance().getScrollPaneHeight();
+
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        int imageWidth = getParent().getWidth();
+        int imageHeight = getParent().getHeight();
+        int numXRepeats = (panelWidth / imageWidth) + 1;
+        int numYRepeats = (panelHeight / imageHeight) + 1;
+
+        // Draw background
+        for (int y = 0; y < numYRepeats; y++) {
+            for (int x = 0; x < numXRepeats; x++) {
+                if (backgroundImage != null) {
+                    g.drawImage(backgroundImage, x * imageWidth, 0, imageWidth, imageHeight, this);
+                }
+            }
+        }
+
         // Draw the player
         Player player = GameData.getInstance().getPlayer();
-//        System.out.println("player x before drawing:" + player.getX());
-//        System.out.println("player y before drawing:" + player.getY());
         player.draw(g);
-//        System.out.println("scroll height: " + scrollPaneHeight);
-        for (Building building : buildings) {
 
+        // Draw buildings
+        int x = 20;
+        int scrollPaneHeight = GameData.getInstance().getScrollPaneHeight();
+        for (Building building : buildings) {
             int width = building.getBreadth();
             int height = (int) (building.getLength());
+
+
             g.setColor(Color.YELLOW);
-            g.fillRect(x, scrollPaneHeight - height , width, height);
+            g.fillRect(x, scrollPaneHeight - height, width, height);
+
             g.setColor(Color.RED);
             g.drawString("Building", x + 10, scrollPaneHeight - height - 20);
             g.drawString("Length: " + building.getLength(), x + 10, scrollPaneHeight - height);
             g.drawString("Breadth: " + building.getBreadth(), x + 10, scrollPaneHeight - height + 20);
 
+            String equation = Equations.getEquation(x / (building.getBreadth() + 70) + 1);
+            g.drawString(equation, x + 10, scrollPaneHeight - height + 40);
 
-            if (x1<131){
-                String equation = Equations.getEquation(x1);
-                g.drawString(equation,  x + 10, scrollPaneHeight - height + 40);
-            }
-            x1++;
             x += width + 70; // Add a gap between buildings
         }
     }
